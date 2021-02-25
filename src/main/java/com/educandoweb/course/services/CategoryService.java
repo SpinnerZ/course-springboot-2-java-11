266cfbb95174dcc8d9314e9bf22cfdd85fda6fd8
Leonardo A. de Souza <leo26gpl@gmail.com>
2021-02-25 06:47:13 -0300

    Category

---
+++ src/main/java/com/educandoweb/course/services/CategoryService.java
@@ -0,0 +1,25 @@
+package com.educandoweb.course.services;
+
+import com.educandoweb.course.entities.Category;
+import com.educandoweb.course.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class CategoryService {
+
+    @Autowired
+    private CategoryRepository repository;
+
+    public List<Category> findAll() {
+        return repository.findAll();
+    }
+
+    public Category findById(Long id) {
+        Optional<Category> obj = repository.findById(id);
+        return obj.get();
+    }
+}
